c22ca2a4665b46fbc7e7fb093eb60153550948f1
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-03 21:55:59 -0300

    solve questions

--- Python/2Sem/Exercicios_praticos/Pasta07_Matrizes_Intermediario/Ex03_Estoque_atual.py
+++ Python/2Sem/Exercicios_praticos/Pasta07_Matrizes_Intermediario/Ex03_Estoque_atual.py
@@ -8,12 +8,34 @@ d.	Qual o custo total de cada produto
 e.	Qual o custo total de cada armazém
 '''
 
-dados = [[0] * 5 for i in range(3)]
+dados = [[0] * 3 for i in range(5)]
 produtos = [0] * 3
 
-for i in range(3):
-    for j in range(4):
-        dados[i][j] = int(input(f'Informe a quantidade do produto {i + 1} no armazém {j + 1}: '))
+for col in range(3):
+    for lin in range(5):
+        if lin == 4:
+            dados[4][col] = int(input(f'Informe o custo do produto: {col + 1}'))
+        else:
+            dados[lin][col] = int(input(f'Informe a quantidade do produto {col + 1} no armazém {lin + 1}: '))
+    #    dados[i][5] += dados[i][j]
+
+
+#    dados[5][col] = int(input('Informe o custo do produto: '))
+
+
+for i in range(5):
+    for j in range(3):
+        print(dados[i][j], end=' \t')
+    print()
+
+
+
+
+
+
+
+'''
+    dados[i][j] = int(input(f'Informe a quantidade do produto {i + 1} no armazém {j + 1}: '))
     dados[i][4] = int(input(f'Informe o custo do produto {i + 1}: '))
     produtos[i] = sum(dados[i][0:4])
     soma_prod1 = sum(dados[0][0:4])
@@ -24,4 +46,4 @@ print(f'Quantidade de itens armazenados em cada armazém: {produtos}')
 print(f'Armazém com maior estoque do produto 2: {max(dados[1][0:4])}')
 print(f'Armazém com menor estoque do produto 2: {min(dados[1][0:4])}')
 print(f'Custo total de cada produto: {produtos}')
-print(f'Custo total de cada armazém: {soma_prod1}, {soma_prod2}, {soma_prod3}')
+print(f'Custo total de cada armazém: {soma_prod1}, {soma_prod2}, {soma_prod3}')'''
