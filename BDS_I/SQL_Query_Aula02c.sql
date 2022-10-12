a4526951ae4777abf4eebaff40e9870c6fc155cf
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-11 22:00:57 -0300

    .

---
+++ BDS_I/SQL_Query_Aula02c.sql
@@ -0,0 +1,10 @@
+USE MinhaCaixa 
+
+SELECT * FROM Clientes AS Cli --As linhas onde o sexo é masculino e todas as colunas
+WHERE ClienteSexo = 'M' 
+    AND (ClienteBairro = 'CENTRO' OR ClienteBairro = 'BOM RETIRO')
+    AND ClienteEstadoCivil = 'S'
+
+SELECT ClienteNome FROM Clientes --Todas as linhas e só a coluna ClienteNome
+
+SELECT ClienteNome, ClienteSobrenome FROM Clientes --Todas as linhas e as colunas 
