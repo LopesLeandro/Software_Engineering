6900cd0572a9200771eefa30d9a03b4fe052a8fd
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-12 21:49:56 -0300

    Update Class01_Database.sql

--- MySQL/CursoVideo/Class01_Database.sql
+++ MySQL/CursoVideo/Class01_Database.sql
@@ -6,8 +6,14 @@ Abstraction:  Paper form == enregistrements
               Dossier == Tables
               Cabinet == Archives
 How files were stored: Sequel Archives;
-
+Why the US army needed a way to storage data;
+About COBOL language and when it started;
 What`s database;
-
+Database, management system and exploration language;
+US Army and IBM starts creating hierarchical database;
+First netwarok model and a new way to storage data;
+Sequel -- SQL -- Seek well -- bad joke;
+Framework ANSI and ISO;
+A lot of company and they own database: Oracle, IBM, dBASE, SQL Server, Maria DB, Postgree, MySQL and FireBird;
 
 '''
