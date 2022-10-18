0bb951f45f9f4245a45277b21b5e60f160be2c30
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-18 13:17:58 -0300

    new python test

--- Python/2Sem/Exercicios_praticos/Pasta08_Funcoes_basico I [E10]/Ex02_Convertendo_tempo.py
+++ Python/2Sem/Exercicios_praticos/Pasta08_Funcoes_basico I [E10]/Ex02_Convertendo_tempo.py
@@ -4,13 +4,11 @@ Todas as variáveis devem ser passadas como parâmetro, não havendo variáveis 
 a.	1h = 3600s
 b.	1m = 60s
 '''
-valor_segundos = int(input('Digite o valor de segundos: '))
+
 def funcao_conversor (segundos:int):
     horas = segundos // 3600
     minutos = (segundos % 3600) // 60
     segundos = (segundos % 3600) % 60
 
-
-print(funcao_conversor('{horas}: {minutos}: {segundos}'))
-
+valor_segundos = int(input('Digite o valor de segundos: '))
 print(funcao_conversor(valor_segundos))
--- Python/2Sem/Exercicios_praticos/Pasta08_Funcoes_basico I [E10]/Ex03_Notas_Medias.py
+++ Python/2Sem/Exercicios_praticos/Pasta08_Funcoes_basico I [E10]/Ex03_Notas_Medias.py
@@ -3,3 +3,23 @@
 Se a letra for A o procedimento calcula a média aritmética das notas do aluno, se for P o procedimento calcula a média ponderada com os pesos 5,3 e 2. 
 A média calculada deve ser devolvida ao programa principal para, então, ser mostrada.
 '''
+
+def f (nota_a:float, nota_b:float, nota_c:float):
+    letra = input('Digite a letra A ou P: ')
+    if letra == 'A':
+        media_aritimetica = (nota_a + nota_b + nota_c) / 3
+    if letra == 'P':
+        media_ponderada = ((nota_a * 5) + (nota_b * 3) + (nota_c * 2)) / 3
+    return media_aritimetica,media_ponderada
+
+nota_a = float(input('Digite a nota 1: '))
+nota_b = float(input('Digite a nota 2: '))
+nota_c = float(input('Digite a nota 3: '))
+print('Notas 1, 2 e 3 deste aluno: ', nota_a,', ', nota_b,', ',  nota_c)
+
+
+
+
+#print(f('A média aritmética do aluno é: {media_aritimetica}'))
+#print(f('A média ponderada do aluno é: {media_ponderada}'))
+
