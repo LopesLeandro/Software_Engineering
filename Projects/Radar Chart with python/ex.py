57d7338ebb34dd5177f7e6439bca628da9293366
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-20 07:27:08 -0300

    I love it

--- Projects/Radar Chart with python/ex.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from soccerplots.radar_chart import Radar
--- Python/2Sem/Exercicios_praticos/Pasta09_Funcoes_Intermediario [E11]/Ex03_Senso_prefeitura.py
+++ Python/2Sem/Exercicios_praticos/Pasta09_Funcoes_Intermediario [E11]/Ex03_Senso_prefeitura.py
@@ -4,8 +4,65 @@ Faça uma função que leia esses dados para um número não determinado de pess
 a média do número de filhos, o maior salário e o percentual de pessoas com salário até R$ 350,00.
 '''
 
-def dados():
-    while True:
+#FUNÇÃO PARA NÚMERO DE FAMÍLIAS A COLETAR DADOS
+from math import floor
+
+
+def habitantes():
+    num_habitantes = int(input('Digite o número de habitantes: '))
+    return num_habitantes
+
+#FUNÇÃO COLETA SALARIO E NÚMERO DE FILHOS
+def dados(num_habitantes):
+    salario = []
+    filhos = []
+    for i in range(num_habitantes):
+        salario.append(float(input('Digite o salário: R$ ')))
+        filhos.append(int(input('Digite quantidade de filhos: ')))
+    return salario, filhos
+
+
+#FUNÇÃO QUE CALCULA A MÉDIA DE SALÁRIO DA POPULAÇÃO E A MÉDIA DO NÚMERO DE FILHOS
+def f_media(salario, filhos, num_habitantes):
+    soma_salario = 0
+    soma_filhos = 0
+    for i in range(num_habitantes):
+        soma_salario += salario[i]
+        soma_filhos += filhos[i]
+    media_salario = soma_salario / num_habitantes
+    media_filhos = soma_filhos / num_habitantes
+    return media_salario, media_filhos
+
+#FUNÇÃO QUE CALCULA O MAIOR SALÁRIO
+def f_maior_salario(salario, num_habitantes):
+    maior = 0
+    for i in range(num_habitantes):
+        if maior < salario[i]:
+            maior = salario[i]
+    return maior
+
+#FUNÇÃO QUE CALCULA O PERCENTUAL DE PESSOAS COM SALÁRIO SUPERIOR A R$ 350,00
+
+#FUNÇÃO PRINCIPAL QUE IMPRIME E ORGANIZA AS FUNÇÕES
+def senso():
+    num_habitantes = habitantes()
+    salario, filhos = dados(num_habitantes)
+    media_salario, media_filhos = f_media(salario, filhos, num_habitantes)
+    maior_salario = f_maior_salario(salario, num_habitantes)
+    print(salario, filhos, media_salario, floor(media_filhos), maior_salario.__format__('.2f'))
+
+#IMPRIME TUDO
+senso()
+
+
+
+'''
+def pessoas():
+    num_pessoas = int(input("Digite o número de pessoas: "))
+    return num_pessoas
+
+def dados(num_pessoas):
+    for i in range(num_pessoas):
         salario = float(input("Digite o salário: "))
         if salario == 0:
             break
@@ -29,6 +86,7 @@ def percentual(salario, filhos):
     return percentual
 
 def senso_prefeitura():
+    num_pessoas = pessoas()
     salario, filhos = dados()
     media_salario = media_salario(salario, filhos)
     media_filhos = media_filhos(salario, filhos)
@@ -38,3 +96,6 @@ def senso_prefeitura():
     print("A média do número de filhos é: ", media_filhos.__format__(".2f"))
     print("O maior salário é: ", maior_salario.__format__(".2f"))
     print("O percentual de pessoas com salário até R$ 350,00 é: ", percentual.__format__(".2f"))
+
+senso_prefeitura()
+'''
