63c1f14d76999420764b4f79b5be65101d65affa
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-10-24 12:50:13 -0300

    game_timer_py

--- Python/2Sem/Exercicios_praticos/Pasta10_Funcoes_avancadas [E12]/Ex05_Tempo_jogo.py
+++ Python/2Sem/Exercicios_praticos/Pasta10_Funcoes_avancadas [E12]/Ex05_Tempo_jogo.py
@@ -3,3 +3,19 @@
 A função deverá retornar a duração expressa em minutos, considerando que o tempo máximo de duração de um jogo é de 24 horas e que ele pode começar em um dia e terminar no outro.
 '''
 
+def f_tempo_jogo(hora_inicio, minuto_inicio, hora_fim, minuto_fim):
+    if hora_inicio == hora_fim:
+        return minuto_fim - minuto_inicio
+    else:
+        if hora_fim < hora_inicio:
+            return (((hora_inicio - hora_fim) * 60) - 1440) * -1 + (minuto_fim - minuto_inicio)
+        return (hora_fim - hora_inicio) * 60 + (minuto_fim - minuto_inicio)
+
+def calc():
+    hora_inicio = int(input('Digite a hora de início: '))
+    minuto_inicio = int(input('Digite os minutos de início: '))
+    hora_fim = int(input('Digite a hora de término: '))
+    minuto_fim = int(input('Digite os minutos de término: '))
+    print(f'A duração do jogo foi de {f_tempo_jogo(hora_inicio, minuto_inicio, hora_fim, minuto_fim)} minutos.')
+
+calc()
