2978e5ff172e54beb13c65b4dc1332480771ba99
Leandro Lopes <101912142+LopesLeandro@users.noreply.github.com>
2022-11-10 21:09:28 -0300

    .

---
+++ R/Ex22_Atividade_Aula.12.R
@@ -0,0 +1,36 @@
+# Probabilidades
+
+#1. Apresente os espaços amostrais dos seguintes experimentos aleatórios:
+#a) Lançamento de uma moeda honesta e observação da face voltada para cima.
+    # S= {cara, coroa}
+
+#b) Contagem do número de clientes numa fila única do banco, que chegaram durante uma hora.
+    # S= {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, ...}
+
+#c) Medição da velocidade do vento, em km/h, na pista de um aeroporto.
+    # S= { 0 < x <= 560 } -- Em km/h
+
+#d) Medição da temperatura, em graus Celsius, numa estação meteorológica da cidade de Joinville.
+    # S= { 1.45 < x <= 42.4 } -- Em graus Celsius
+
+#2. Considere que você vai cronometrar o tempo, em segundos, para carregar uma página na web. Represente, em forma de conjuntos, os seguintes eventos:
+#a) A = mais do que 5 e, no máximo, 10 segundos
+    # A = { 5 < x <= 10 }
+
+#b) B = Mais do que 10 segundos
+    # B = { 10 < x }
+
+#c) C = Mais do que 8 segundos
+    # C = { 8 < x }
+
+#d) D= A U B
+    # D = { 6,7,8,9,10,11,... }
+
+#e) E= A /\ B
+    # E = { }
+
+#f) F = A /\ C
+    # F = { 9,10 }
+
+#g) G = A’
+    # G = { 0,1,2,3,4,5,11,12, ... }
